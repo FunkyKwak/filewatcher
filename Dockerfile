be1a2d57f8b4a2f9a42555f9284abf1debdb460b
Yann <crazy_rocker78@hotmail.fr>
2026-03-01 06:53:22 +0100

    Premier commit - non testé

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine:latest
+
+RUN apk add --no-cache inotify-tools msmtp mailx bash
+RUN apt-get install curl
+
+ARG TELEGRAM_BOT_TOKEN
+ARG TELEGRAM_CHAT_ID
+
+WORKDIR /app
+COPY watch.sh /app/watch.sh
+RUN chmod +x /app/watch.sh TELEGRAM_BOT_TOKEN="$TELEGRAM_BOT_TOKEN" TELEGRAM_CHAT_ID="$TELEGRAM_CHAT_ID"
+
+CMD ["/app/watch.sh"]
---
+++ watch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Source - https://stackoverflow.com/a/43008569
+# Posted by JRichardsz, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-03-01, License - CC BY-SA 4.0
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+# from this line, you could use your variables as you need
+
+#WATCH_DIR="/watch_dir"
+echo "Surveillance démarrée sur $WATCH_DIR"
+
+inotifywait -m -r -e delete --format '%f' "$WATCH_DIR" | while read FILE
+do
+    EXT="${FILE##*.}"
+    EXT_LOWER=$(echo "$EXT" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$EXT_LOWER" != "xmp" ]; then
+        MESSAGE="ALERTE NAS - Suppression détectée : $FILE"
+        curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="$MESSAGE" > /dev/null
+    fi
+done
